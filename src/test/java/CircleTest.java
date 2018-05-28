f149401a6856f741739017987886145666b69dde
kpo <Pkartike@thoughtworks.com>
2018-05-28 20:47:44 +0530

    shouldReturnAreaOfCircle test

--- src/test/java/CircleTest.java
+++ src/test/java/CircleTest.java
@@ -12,4 +12,10 @@ public class CircleTest {
         assertEquals(3.14, circle.calculateArea(), 0.001);
     }
 
+    @Test
+    public void shouldReturnAreaOfCircle() {
+        Circle circle = new Circle(3.0);
+
+        assertEquals(28.25, circle.calculateArea(), 0.0099999999999998);
+    }
 }
