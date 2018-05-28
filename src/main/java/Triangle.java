86192524ad55aa9b0afcf7bceeb0f72f389e8e65
kpo <Pkartike@thoughtworks.com>
2018-05-28 21:23:34 +0530

    returnPerimeterOfRightTriangle test

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,13 +1,19 @@
 public class Triangle {
     private double base;
     private double height;
+    private double hypotenuse;
 
-    public Triangle(double base, double height) {
+    public Triangle(double base, double height, double hypotenuse) {
         this.base = base;
         this.height = height;
+        this.hypotenuse = hypotenuse;
     }
 
     public double calculateArea() {
         return 0.5 * base * height;
+    }
+
+    public double calculatePerimeter() {
+        return base + height + hypotenuse;
     }
 }
--- src/test/java/TriangleTest.java
+++ src/test/java/TriangleTest.java
@@ -5,8 +5,16 @@ import static org.junit.Assert.assertEquals;
 public class TriangleTest {
 
     @Test
-    public void returnAreaOfTriangle() {
-        Triangle triangle = new Triangle(12.0,5.0);
+    public void returnAreaOfRightTriangle() {
+        Triangle triangle = new Triangle(12.0,5.0, 9.0);
+
         assertEquals(30.0, triangle.calculateArea(), 0.0001);
+    }
+
+    @Test
+    public void returnPerimeterOfRightTriangle() {
+        Triangle triangle = new Triangle(12.0,5.0,9.0);
+
+        assertEquals(26.0, triangle.calculatePerimeter(), 0.0001);
     }
 }
